fb4e48505a5f75bc918782af0f160b52f3229618
lucas8x <lucasemanuel8x@gmail.com>
2026-06-27 13:01:45 -0300

    feat: replace bmp-js with @huh-david/bmp-js for BMP processing

--- src/image/getSharpInstance.ts
+++ src/image/getSharpInstance.ts
@@ -1,5 +1,5 @@
 import fs from 'node:fs/promises';
-import bmp from 'bmp-js';
+import { sharpFromBmp } from '@huh-david/bmp-js/sharp';
 import sharp from 'sharp';
 import { cbzAnimationSupport } from '../cli/args';
 
@@ -8,20 +8,7 @@ sharp.cache(false);
 export async function getSharpInstance(filePath: string): Promise<sharp.Sharp> {
   if (filePath.endsWith('.bmp')) {
     const bmpFile = await fs.readFile(filePath);
-
-    const { data, width, height } = bmp.decode(bmpFile);
-
-    for (let i = 0; i < data.length; i += 4) {
-      const b = data[i];
-      const r = data[i + 2];
-
-      data[i] = r; // R
-      data[i + 2] = b; // B
-    }
-
-    return sharp(data, {
-      raw: { width, height, channels: 4 },
-    });
+    return sharpFromBmp(bmpFile);
   }
 
   return sharp(filePath, {
