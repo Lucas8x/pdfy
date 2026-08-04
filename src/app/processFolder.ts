Lucas8x/pdfy

commit a04bdd153d3856f7953f00226eec13ae73ba53e4

refactor: move processFolder to a dedicated file

src/app/processFolder.ts
@@ -1,0 +1,78 @@
+#!/usr/bin/env node
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { enableCBZ, outputPath } from '../cli/args';
+import { processImages } from '../image/processImages';
+import { createCBZ } from '../output/createCBZ';
+import { createPDF } from '../output/createPDF';
+import { makeClickablePath } from '../utils';
+import { printOutputDetails } from '../utils/printOutputDetails';
+import { readFolder } from '../utils/readFolder';
+
+export async function processFolder(folderPath: string, password?: string) {
+  console.log(
+    `📂 Initiating process in: ${makeClickablePath(folderPath).ansi}`
+  );
+
+  const files = await readFolder(folderPath);
+
+  if (!files.length) {
+    console.error(
+      `⚠️ No valid images found in [${path.basename(folderPath)}]. PDF/CBZ creation aborted.`
+    );
+    return;
+  }
+
+  const outputFilename = path
+    .basename(folderPath)
+    .concat(enableCBZ ? '.cbz' : '.pdf');
+
+  const finalOutputPath = path.join(outputPath, outputFilename);
+
+  let cbz: ReturnType<typeof createCBZ> | null = null;
+  let pdf: ReturnType<typeof createPDF> | null = null;
+
+  if (enableCBZ) {
+    const stats = await fs.stat(folderPath);
+    cbz = createCBZ(finalOutputPath, {
+      birthtime: stats.birthtime,
+      mtime: stats.mtime,
+      imagesLength: files.length,
+    });
+  } else {
+    pdf = createPDF(finalOutputPath, password);
+  }
+
+  const padMax = [...files.length.toString()].length;
+
+  for await (const file of processImages(files)) {
+    const filename = String(file.index + 1)
+      .padStart(padMax, '0')
+      .concat(
+        file.extension.startsWith('.') ? file.extension : `.${file.extension}`
+      );
+
+    if (file.buffer) {
+      if (enableCBZ) {
+        cbz?.append(file.buffer, filename);
+      } else {
+        pdf?.append(file.buffer, file.width, file.height);
+      }
+    }
+
+    if (file?.useCopyInstead && enableCBZ) {
+      cbz?.copy(file.path, filename);
+    }
+
+    file.buffer = null;
+  }
+
+  await (cbz || pdf)?.finalize();
+
+  let totalOriginalSize = 0;
+  for (const file of files) {
+    totalOriginalSize += file.size;
+  }
+
+  await printOutputDetails(finalOutputPath, totalOriginalSize);
+}

src/index.ts
@@ -1,88 +1,8 @@
 #!/usr/bin/env node
-import fs from 'node:fs/promises';
-import path from 'node:path';
-import {
-  enableCBZ,
-  inputPath,
-  outputPath,
-  password as pwArg,
-} from './cli/args';
+import { processFolder } from './app/processFolder';
+import { enableCBZ, inputPath, password as pwArg } from './cli/args';
 import { askPassword } from './cli/askPasswor';
 import { selectFolder } from './cli/selectFolder';
-import { processImages } from './image/processImages';
-import { createCBZ } from './output/createCBZ';
-import { createPDF } from './output/createPDF';
-import { makeClickablePath } from './utils';
-import { printOutputDetails } from './utils/printOutputDetails';
-import { readFolder } from './utils/readFolder';
-
-async function processFolder(folderPath: string, userPassword: string) {
-  console.log(
-    `📂 Initiating process in: ${makeClickablePath(folderPath).ansi}`
-  );
-
-  const files = await readFolder(folderPath);
-
-  if (!files.length) {
-    console.error(
-      `⚠️ No valid images found in [${path.basename(folderPath)}]. PDF/CBZ creation aborted.`
-    );
-    return;
-  }
-
-  const outputFilename = path
-    .basename(folderPath)
-    .concat(enableCBZ ? '.cbz' : '.pdf');
-
-  const finalOutputPath = path.join(outputPath, outputFilename);
-
-  let cbz: ReturnType<typeof createCBZ> | null = null;
-  let pdf: ReturnType<typeof createPDF> | null = null;
-
-  if (enableCBZ) {
-    const stats = await fs.stat(folderPath);
-    cbz = createCBZ(finalOutputPath, {
-      birthtime: stats.birthtime,
-      mtime: stats.mtime,
-      imagesLength: files.length,
-    });
-  } else {
-    pdf = createPDF(finalOutputPath, userPassword);
-  }
-
-  const padMax = [...files.length.toString()].length;
-
-  for await (const file of processImages(files)) {
-    const filename = String(file.index + 1)
-      .padStart(padMax, '0')
-      .concat(
-        file.extension.startsWith('.') ? file.extension : `.${file.extension}`
-      );
-
-    if (file.buffer) {
-      if (enableCBZ) {
-        cbz?.append(file.buffer, filename);
-      } else {
-        pdf?.append(file.buffer, file.width, file.height);
-      }
-    }
-
-    if (file?.useCopyInstead && enableCBZ) {
-      cbz?.copy(file.path, filename);
-    }
-
-    file.buffer = null;
-  }
-
-  await (cbz || pdf)?.finalize();
-
-  let totalOriginalSize = 0;
-  for (const file of files) {
-    totalOriginalSize += file.size;
-  }
-
-  await printOutputDetails(finalOutputPath, totalOriginalSize);
-}
 
 async function main() {
   const selectedFolders =

commit a63d7792a72f4be456b50ad0951867657923dc1a

fix: sort regression in a04bdd1
+ remove unnecessary shebang

src/app/processFolder.ts
@@ -1,7 +1,6 @@
-#!/usr/bin/env node
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import { enableCBZ, outputPath } from '../cli/args';
+import { enableCBZ, outputPath, sort } from '../cli/args';
 import { processImages } from '../image/processImages';
 import { createCBZ } from '../output/createCBZ';
 import { createPDF } from '../output/createPDF';
@@ -14,7 +13,7 @@ export async function processFolder(folderPath: string, password?: string) {
     `📂 Initiating process in: ${makeClickablePath(folderPath).ansi}`
   );
 
-  const files = await readFolder(folderPath);
+  const files = await readFolder(folderPath, sort);
 
   if (!files.length) {
     console.error(